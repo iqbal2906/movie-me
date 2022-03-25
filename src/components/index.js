b320afa0511db1daebf69049a320fe3c93140cce
Iqbal Sahrizal <54559728+iqbal2906@users.noreply.github.com>
2022-03-25 23:40:22 +0700

    make movieCard

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Button } from "./button/Button";
 export { default as Modal } from "./modal/Modal";
 export { default as HeroSlide } from "./hero-slide/HeroSlide";
 export { default as MovieList } from "./movie-list/MovieList";
+export { default as MovieCard } from "./movie-card/MovieCard";
