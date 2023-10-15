712470c51a2a369ff4c126ac4062b41a7b2c01ff
Iqbal Sahrizal <iqbal.sahrizal@dompetkilat.co.id>
2023-10-15 11:00:41 +0700

    change apikey

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "dd89841b7a53b045472cfc4f3aa56ab9",
+  apiKey: "a30de5cd4546e069dc49d498fe2a5ff6",
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
